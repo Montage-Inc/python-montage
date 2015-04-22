3971e48c72516bf24b0b87a6a02180a7a26a252b
dmpayton <derek.payton@gmail.com>
2015-04-21 22:36:04 -0700

    Various fixes to get things working (since I don't have tests -- yet).

--- montage.py
+++ montage.py
@@ -10,27 +10,24 @@ __version__ = '1.0.0'
 
 class MontageAPI(object):
     domain = 'dev.montagehot.club'
+    endpoints = {
+        'auth': 'auth/',
+        'schema-list': 'schemas/',
+        'schema-detail': 'schemas/{schema}/',
+        'document-query': 'schemas/{schema}/query/',
+        'document-save': 'schemas/{schema}/save/',
+        'document-detail': 'schemas/{schema}/{document_id}/',
+        'file-list': 'files/',
+        'file-detail': 'files/{file_id}',
+    }
 
     def __init__(self, subdomain, token=None):
         self.subdomain = subdomain
         self.token = token
 
-    @cached_property
-    def endpoints(self):
-        return {
-            'auth': 'auth/',
-            'schema-list': 'schemas/',
-            'schema-detail': 'schemas/{schema}/',
-            'document-query': 'schemas/{schema}/query/',
-            'document-save': 'schemas/{schema}/save/',
-            'document-detail': 'schemas/{schema}/{document_id}/',
-            'file-list': 'files/',
-            'file-detail': 'files/{file_id}',
-        }
-
     def url(self, name, **kwargs):
         endpoint = self.endpoints[name].format(**kwargs)
-        return 'https://{subdomain}.{domain}/api/v1/{endpoint}'.format(
+        return 'http://{subdomain}.{domain}/api/v1/{endpoint}'.format(
             subdomain=self.subdomain,
             domain=self.domain,
             endpoint=endpoint,
@@ -39,25 +36,24 @@ class MontageAPI(object):
     def get_headers(self):
         headers = {
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
             'User-Agent': 'Montage Python v{0}'.format(__version__),
         }
         if self.token:
-            headers['Authorization'] = 'Token {0}'.format(self.token),
+            headers['Authorization'] = 'Token {0}'.format(self.token)
         return headers
 
     def request(self, url, method=None, **kwargs):
         method = method or 'get'
 
-        headers = kwargs.pop('headers', {})
-        headers.update(self.get_headers())
+        headers = self.get_headers()
+        headers.update(kwargs.pop('headers', {}))
 
         response = requests.request(method, url, headers=headers, **kwargs)
         return response.json()
 
     def authenticate(self, email, password):
         url = self.url('auth')
-        response = self.request(url, data={
+        response = self.request(url, 'post', data={
             'username': email,
             'password': password
         })
@@ -90,15 +86,20 @@ class Documents(object):
         self.api = schema.api
 
     def query(self):
-        return Query(self.schema, self.api)
+        return Query(self.schema)
 
     def save(self, *documents):
         data = documents[0] if len(documents) == 1 else documents
         url = self.api.url('document-save', schema=self.name)
         return self.api.request(url, 'post', json=data)
 
+    def get(self, document_id):
+        url = self.api.url('document-detail', schema=self.schema.name,
+            document_id=document_id)
+        return self.api.request(url).get('data')
+
     def delete(self, document_id):
-        url = self.api.url('document-delete', schema=self.schema.name,
+        url = self.api.url('document-detail', schema=self.schema.name,
             document_id=document_id)
         return self.api.request(url, 'delete')
 
@@ -114,13 +115,16 @@ class Query(object):
 
     def __init__(self, schema, **kwargs):
         self.schema = schema
-        self.descriptor = self.QueryDescriptor(**kwargs)
+        self.api = schema.api
+        self.descriptor = self.get_descriptor(**kwargs)
 
     def __iter__(self):
         # Use a session to keep the connection open
-        session = requests.Session(headers=self.get_headers())
+        session = requests.Session()
+        session.headers.update(self.api.get_headers())
+
         url = self.api.url('document-query', schema=self.schema.name)
-        response = session.post(url, json=self.descriptor._asdict())
+        response = session.post(url, json=self.descriptor._asdict()).json()
 
         # Yield the initial result set
         for document in response['data']:
@@ -131,12 +135,23 @@ class Query(object):
             url = self.api.url('document-query', schema=self.schema.name)
             response = session.get(url, params={
                 'cursor': response['cursors']['next']
-            })
+            }).json()
 
             for document in response['data']:
                 yield document
 
         session.close()
+
+    def get_descriptor(self, **kwargs):
+        defaults = {
+            'filter': {},
+            'limit': None,
+            'offset': None,
+            'order_by': None,
+            'ordering': 'asc',
+        }
+        defaults.update(**kwargs)
+        return self.QueryDescriptor(**defaults)
 
     def clone(self, **kwargs):
         descriptor = copy.deepcopy(self.descriptor._asdict())
