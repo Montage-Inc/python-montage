3fa80b1c6a8bcb079cc6744be37609a1a1702ceb
dmpayton <derek.payton@gmail.com>
2015-04-22 02:45:01 -0700

    Add a smart JSON encoder that can handle datetimes and decimals.

--- montage.py
+++ montage.py
@@ -2,10 +2,50 @@
 
 import collections
 import copy
+import datetime
+import decimal
+import json
 import requests
 from cached_property import cached_property
 
 __version__ = '1.0.0'
+
+
+def is_aware(value):
+    """
+    Determines if a given datetime.datetime is aware.
+    The logic is described in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+    """
+    return value.tzinfo is not None and value.tzinfo.utcoffset(value) is not None
+
+
+class SmartJSONEncoder(json.JSONEncoder):
+    """
+    JSONEncoder subclass that knows how to encode date/time and decimal types.
+    """
+    def default(self, o):
+        # See "Date Time String Format" in the ECMA-262 specification.
+        if isinstance(o, datetime.datetime):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith('+00:00'):
+                r = r[:-6] + 'Z'
+            return r
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.time):
+            if is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(o, decimal.Decimal):
+            return str(o)
+        else:
+            return super(SmartJSONEncoder, self).default(o)
 
 
 class MontageAPI(object):
@@ -47,6 +87,10 @@ class MontageAPI(object):
 
         headers = self.get_headers()
         headers.update(kwargs.pop('headers', {}))
+
+        data = kwargs.pop('json', None)
+        if data:
+            kwargs['data'] = json.dumps(data, cls=SmartJSONEncoder)
 
         response = requests.request(method, url, headers=headers, **kwargs)
         return response.json()
