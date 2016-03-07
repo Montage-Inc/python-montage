ccd8101c32e3f47ae94b44c602c5ab066842394e
dmpayton <derek.payton@gmail.com>
2016-03-06 23:41:54 -0800

    Test invalid authentication and tweak tox.ini

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import json
 import montage
 import responses
 
@@ -20,9 +21,29 @@ class ClientTests(MontageTests):
 
         assert client.token is None
 
-        client.authenticate('test@example.com', 'letmein')
+        authenticated = client.authenticate('test@example.com', 'letmein')
 
-        assert self.client.token == USER['token']
+        assert authenticated is True
+        assert client.token == USER['token']
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == endpoint
+
+    @responses.activate
+    def test_authenticate_invalid(self):
+        error = {'errors': [{'detail': 'Incorrect authentication credentials.'}]}
+        endpoint = 'https://testco.hexxie.com/api/v1/auth/'
+        responses.add(responses.POST, endpoint, body=json.dumps(error),
+            content_type='application/json')
+
+        client = montage.Client('testco', url='hexxie.com')
+
+        assert client.token is None
+
+        authenticated = client.authenticate('fake@example.com', 'invalid')
+
+        assert authenticated is False
+        assert client.token is None
 
         assert len(responses.calls) == 1
         assert responses.calls[0].request.url == endpoint
