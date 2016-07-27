5e8b4fb671ab8efa59fa1bce695e86b15482692b
dmpayton <derek.payton@gmail.com>
2016-07-26 17:10:49 -0700

    Make the response available when raising an HttpError.

--- montage/errors.py
+++ montage/errors.py
@@ -7,6 +7,11 @@ class MontageError(Exception):
 
 
 class HttpError(MontageError):
-    def __init__(self, status_code, message):
-        self.status_code = status_code
+    def __init__(self, response, message):
+        self.response = response
         self.message = message
+
+    @property
+    def status_code(self):
+        return self.response.status_code
+
